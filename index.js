642637d36f5f518cde31c256584a6d980a9540b7
Kardo J <117020680+KJankowitz@users.noreply.github.com>
2024-01-21 19:58:48 +0200

    Clean up syntax

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
         checkSound(buttonNum);
         buttonPress(buttonNum);
     } );
-}
+};
 
 // Keyboard buttons 
 document.addEventListener("keydown", function(event) {
     checkSound(event.key);
     buttonPress(event.key);
-})
+});
 
 //Play the sound
 function checkSound (key) {
     switch (key) {
         case "w":
             var crash = new Audio("sounds/crash.mp3");
-            crash.play()
+            crash.play();
             break;
         case "a":
             var tom1 = new Audio("sounds/tom-1.mp3");
@@ -26,29 +26,29 @@ function checkSound (key) {
             break;
         case "s":
             var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play()
+            tom3.play();
             break;
         case "d":
             var kickBass = new Audio("sounds/kick-bass.mp3");
-            kickBass.play()
+            kickBass.play();
             break;
         case "j":
             var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play()
+            tom4.play();
             break;
         case "k":
             var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play()
+            tom2.play();
             break;
         case "l":
             var snare = new Audio("sounds/snare.mp3");
-            snare.play()
+            snare.play();
             break;
       
         default: console.log(buttonNum);
             break;
-      }
-}
+      };
+};
 
 //Animate button pressed
 function buttonPress(currentKey) {
@@ -57,4 +57,4 @@ function buttonPress(currentKey) {
     setTimeout(function() {
         buttonPressed.classList.remove("pressed");
     }, 100);
-}
+};
